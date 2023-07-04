062d9bb57653e53d9d71f860d79330b63712dd62
Shiqi Mei <shiqi.mei@lolimay.cn>
2023-07-05 01:40:16 +0800

    feat: support basic 5 colors messages

--- src/client/AskCodebasePanel.tsx
+++ src/client/AskCodebasePanel.tsx
@@ -1,11 +1,63 @@
 import styles from './AskCodebasePanel.module.scss'
 import { InputBox, WelcomeScreen } from '~/components'
+import { ILog, LogType, LogLevel } from './types'
+import * as cx from 'classnames'
 
 export function AskCodebasePanel() {
+  const logs: ILog[] = [
+    {
+      id: '1',
+      user: { userid: 'test', username: 'test' },
+      type: LogType.UserReq,
+      level: LogLevel.Error,
+      message: 'test',
+      ts: Date.now()
+    },
+    {
+      id: '2',
+      user: { userid: 'test', username: 'test' },
+      type: LogType.UserReq,
+      level: LogLevel.Success,
+      message: 'The command is executed successfully.',
+      ts: Date.now()
+    },
+    {
+      id: '3',
+      user: { userid: 'test', username: 'test' },
+      type: LogType.UserReq,
+      level: LogLevel.Warn,
+      message: 'Warning!!! The command is executed successfully.',
+      ts: Date.now()
+    },
+    {
+      id: '4',
+      user: { userid: 'test', username: 'test' },
+      type: LogType.UserReq,
+      level: LogLevel.Info,
+      message: 'Note!!! The command is executed successfully.\nNote!!! The command is executed successfully.',
+      ts: Date.now()
+    },
+    {
+      id: '5',
+      user: { userid: 'test', username: 'test' },
+      type: LogType.UserReq,
+      level: LogLevel.Log,
+      message: 'Log. The command is executed successfully.',
+      ts: Date.now()
+    }
+  ]
+
   return (
     <div className={styles.AskCodebasePanel}>
-      <WelcomeScreen />
+      {/* <WelcomeScreen /> */}
       <InputBox />
+      <div className={styles.logs}>
+        {logs.map(log => (
+          <div className={cx(styles.log, styles[log.level])} key={log.id}>
+            {log.message}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- src/client/types.ts
+++ src/client/types.ts
@@ -1,15 +1,27 @@
 export enum LogLevel {
-  Log = 'log',
+  Log = 'normal',
   Error = 'error',
   Warn = 'warn',
   Info = 'info',
   Success = 'success'
 }
 
+export enum LogType {
+  UserReq = 'UserReq',
+  ShellResp = 'ShellResp',
+  ChatGPTResp = 'ChatGPTResp'
+}
+
+export interface IUser {
+  userid: string
+  username: string
+}
+
 export interface ILog {
-  id: number
-  ts: number
-  text: string
-  code: number
+  type: LogType
   level: LogLevel
+  id: string
+  ts: number
+  message: string
+  user: IUser
 }
