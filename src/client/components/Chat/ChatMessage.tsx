cf5e06b4da9618ff7bc86c39112ceae18fa84791
Shiqi Mei <shiqi.mei@lolimay.cn>
2023-07-31 14:59:12 +0800

    feat: make chat message font size same to copilot

--- src/client/components/Chat/ChatMessage.tsx
+++ src/client/components/Chat/ChatMessage.tsx
@@ -41,7 +41,7 @@ export const ChatMessage: FC<Props> = memo(({ message, messageIndex }) => {
       )}
       style={{ overflowWrap: 'anywhere' }}
     >
-      <div className='relative m-auto flex p-4 text-base md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl'>
+      <div className='relative m-auto flex p-4 text-base md:gap-6 md:py-6 lg:px-0'>
         <div className='min-w-[40px] text-right font-bold'>
           {message.role === 'assistant' ? <IconRobot size={30} /> : <IconUser size={30} />}
         </div>
