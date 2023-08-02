d78eb1228d9ecbc6543b3b8016dcc42a6bdbb26e
Shiqi Mei <shiqi.mei@lolimay.cn>
2023-08-02 18:20:19 +0800

    chore: update keywords

--- scripts/publish.js
+++ scripts/publish.js
@@ -4,7 +4,9 @@ const execCommand = (command) => {
     execSync(command, { stdio: 'inherit', stderr: 'inherit' })
 }
 
+try {
 execCommand('git add -A');
 execCommand(`git commit -m "bump: prepare for publish"`);
+} catch(e) {}
 execCommand('npm version minor');
 execCommand('git push');
