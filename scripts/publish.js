27d11715f663633924eb98a26781b99166ebb758
Shiqi Mei <shiqi.mei@lolimay.cn>
2023-08-02 18:10:41 +0800

    chore: prepare for publish

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,5 +1,4 @@
 const { execSync } = require('child_process');
-const fs = require('fs');
 
 const execCommand = (command) => {
     execSync(command, { stdio: 'inherit', stderr: 'inherit' })
@@ -7,14 +6,5 @@ const execCommand = (command) => {
 
 execCommand('git add -A');
 execCommand(`git commit -m "chore: prepare for publish"`);
-
 execCommand('npm version minor');
-
-const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
-const version = packageJson.version;
-
-execCommand('git add -A');
-
-execCommand(`git commit -m "bump version to v${version}"`);
-
 execCommand('git push');
